5a2c06135474b18ca0b347cad4282823a2266b75
Ben Hyland <ben.hyland@digital.cabinet-office.gov.uk>
2016-06-21 12:04:01 +0100

    fix remaining test and remove obsolete test

--- test/integration/tagging_to_linkables_test.rb
+++ test/integration/tagging_to_linkables_test.rb
@@ -9,7 +9,6 @@ class TaggingToLinkablesTest < JavascriptIntegrationTest
   test "Tagging to browse pages" do
     edition = FactoryGirl.create(:guide_edition)
     content_id = edition.artefact.content_id
-
     stub_no_links(content_id)
 
     visit edition_path(edition)
@@ -17,36 +16,42 @@ class TaggingToLinkablesTest < JavascriptIntegrationTest
     selectize ['Tax / VAT', 'Tax / RTI (draft)'], 'Mainstream browse pages'
 
     save_tags_and_assert_success
-    assert_publishing_api_patch_links(content_id)
+    assert_publishing_api_patch_links(
+      content_id,
+      {
+        links: {
+          topics: [],
+          mainstream_browse_pages: ["CONTENT-ID-VAT", "CONTENT-ID-RTI"],
+          parent:[]
+        },
+        previous_version: 0
+      }
+    )
   end
 
   test "Tagging to topics" do
     edition = FactoryGirl.create(:guide_edition)
+    content_id = edition.artefact.content_id
+    stub_no_links(content_id)
 
     visit edition_path(edition)
+    switch_tab 'Tagging'
 
-    select 'Oil and Gas / Wells', from: 'Primary topic'
-    select 'Oil and Gas / Fields', from: 'Additional topics'
-    select 'Oil and Gas / Distillation (draft)', from: 'Additional topics'
+    select 'Oil and Gas / Wells', from: 'Parent'
+    select 'Oil and Gas / Fields', from: 'Topics'
+    select 'Oil and Gas / Distillation (draft)', from: 'Topics'
 
-    save_edition_and_assert_success
-    edition.reload
-
-    assert_equal 'oil-and-gas/wells', edition.primary_topic
-    assert_equal ['oil-and-gas/distillation', 'oil-and-gas/fields'], edition.additional_topics
-  end
-
-  test "Mistagging primary and additional topics with the same tag" do
-    edition = FactoryGirl.create(:guide_edition)
-    visit edition_path(edition)
-
-    select 'Oil and Gas / Wells', from: 'Primary topic'
-    select 'Oil and Gas / Wells', from: 'Additional topics'
-    select 'Oil and Gas / Distillation (draft)', from: 'Additional topics'
-
-    save_edition_and_assert_error
-
-    assert page.has_css?('#edition_additional_topics_input.has-error')
-    assert page.has_content?("can't have the primary topic set as an additional topic")
+    save_tags_and_assert_success
+    assert_publishing_api_patch_links(
+      content_id,
+      {
+        links: {
+          topics: ['CONTENT-ID-DISTILL', 'CONTENT-ID-FIELDS'],
+          mainstream_browse_pages: [],
+          parent:['CONTENT-ID-WELLS']
+        },
+        previous_version: 0
+      }
+    )
   end
 end
